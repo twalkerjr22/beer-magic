24ec1534f3a050fababd7b7c576faab2455dae5d
Troy Walker <twalkerjr22@gmail.com>
2019-04-03 14:16:57 -0400

    Updates

---
+++ precache-manifest.f7c7a6155a6cebc1b3ee9b7168e1b9f0.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/beer-magic/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "0a46f97916515ea50a15",
+    "url": "/beer-magic/static/js/runtime~main.388a0ae8.js"
+  },
+  {
+    "revision": "42c6451b142d9b456666",
+    "url": "/beer-magic/static/js/main.127e865f.chunk.js"
+  },
+  {
+    "revision": "26d7f281ca295120a97f",
+    "url": "/beer-magic/static/js/2.157238b2.chunk.js"
+  },
+  {
+    "revision": "42c6451b142d9b456666",
+    "url": "/beer-magic/static/css/main.4a134af8.chunk.css"
+  },
+  {
+    "revision": "26d7f281ca295120a97f",
+    "url": "/beer-magic/static/css/2.6951c225.chunk.css"
+  },
+  {
+    "revision": "8a0e35cff45fb462c8630296bb4f8333",
+    "url": "/beer-magic/index.html"
+  }
+];
